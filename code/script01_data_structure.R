e8369e96ff02ec716ffc8f931ae82933e93f514d
nsaltsgaver <nsaltsgaver1@gmail.com>
2026-08-20 10:47:32 -0400

    august 20th's in-class work

---
+++ code/script01_data_structure.R
@@ -0,0 +1,31 @@
+## data structure
+## scalar
+a <- 2.0 #numeric
+b <- 4L #integer
+
+d <- "aquatic"
+
+a
+b
+d
+
+va <- c(1.0,2.3,3) #numeric vector
+vb <- c("a","b","c") #character vector
+vc <- c("1","2","3")
+
+va
+vb
+vc
+
+##matrix
+va <- c(1.0,2.3,3) #numeric vector
+vb <- c(3,2,5.6) #numeric vector
+
+ma <- cbind(va,vb)
+mb <- rbind(va,vb)
+
+mc <- matrix(c(1,2,3,4),nrow=2,ncol=2)
+
+ma
+mb
+mc
