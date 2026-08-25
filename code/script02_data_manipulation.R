2a79d8902dd230fe8fb9264c7edccc92bbcb70aa
nsaltsgaver <nsaltsgaver1@gmail.com>
2026-08-25 09:55:59 -0400

    finished, up to piping in the assignment. filter / select / mutate functions explored.

---
+++ code/script02_data_manipulation.R
@@ -0,0 +1,38 @@
+library("tidyverse")
+
+set.seed(123)
+
+iris_sub <- as_tibble(iris) %>%
+  group_by(Species) %>%
+  sample_n(3) %>%
+  ungroup
+
+print(iris_sub)
+
+filter(iris_sub, Species == "virginica")
+filter(iris_sub, Species != "virginica")
+
+filter(iris_sub, Species %in% c("virginica","versicolor"))
+filter(iris_sub, Sepal.Width > 3, 
+       Species == "versicolor")
+arrange(iris_sub, desc(Sepal.Length))
+
+select(iris_sub, Sepal.Length)
+select(iris_sub, c(Sepal.Length, Sepal.Width))
+select(iris_sub, -Sepal.Length)
+select(iris_sub, -c(Sepal.Width,Sepal.Length))
+select(iris_sub, starts_with("Sepal"))
+select(iris_sub, -starts_with("Sepal"))
+select(iris_sub, ends_with("Width"))
+select(iris_sub, -ends_with("Length"))
+
+(x_max <- nrow(iris_sub))
+x <- 1:x_max
+mutate(iris_sub, row_id = x)
+mutate(iris_sub, sl_two_times = 2*Sepal.Length)
+
+
+iris_pw <- select(iris_sub, c(Petal.Width, Species))
+iris_petal <- select(iris_pw, starts_with("Petal"))
+iris_pw_two <- mutate(iris_petal, sl_two_times = 2*Petal.Width)
+
